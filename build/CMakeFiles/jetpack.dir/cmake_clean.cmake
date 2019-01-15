09940a0752f5f490572729b594df333617628fc3
sawar7577 <sawa.sagwal@gmail.com>
2019-01-15 14:54:56 +0530

    boilerplate upload

---
+++ build/CMakeFiles/jetpack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jetpack.dir/src/main.cpp.o"
+  "jetpack.pdb"
+  "jetpack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jetpack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
